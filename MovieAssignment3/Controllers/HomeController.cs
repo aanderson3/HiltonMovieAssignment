4ac40fd1ff05fbd4702ab3d6087912d4a2f556aa
aanderson3 <66184238+aanderson3@users.noreply.github.com>
2021-03-24 21:11:30 -0600

    first commit

---
+++ MovieAssignment3/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieAssignment3.Models;
+
+namespace MovieAssignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(Movie oMovie)
+        {
+            TempStorage.NewMovie(oMovie);
+            return View("Confirmation", oMovie);
+        }
+
+        public IActionResult MovieList()
+        {
+            return View(TempStorage.Movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieAssignment3/Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAssignment3.Models
+{
+    public static class TempStorage
+    {
+        private static List<Movie> moviesList = new List<Movie>();
+
+        public static IEnumerable<Movie> Movies => moviesList;
+
+        public static void NewMovie(Movie oMovie)
+        {
+            moviesList.Add(oMovie);
+        }
+    }
+}
